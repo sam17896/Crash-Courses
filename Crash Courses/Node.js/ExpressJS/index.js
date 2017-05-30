e6ce71624a2eec640e91311855777523c80983dd
Ahsan Sohail <k142109@nu.edu.pk>
2017-05-30 16:02:01 +0500

    Node.js: ExpressJS Customer app : Express Validator

--- Crash Courses/Node.js/ExpressJS/index.js
+++ Crash Courses/Node.js/ExpressJS/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+
 
 var path = require('path');
 
@@ -21,24 +23,95 @@ app.use(express.static(path.join(__dirname,'public')));
 
 
 
+//Global vars 
+
+app.use(function(req,res,next){
+	res.locals.errors = null;
+	next();
+});
+
+
+//Express validator 
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+
+
+
+
 //JSON PARSING
 var people = [
 	{
-		name: 'Jeff',
-		age : 30
+		id: 1,
+		firt_name: 'Jeff',
+		last_name : "Doe",
+		email: 'Jeffdoe@gmail.com'
 	},
 	{
-		name: 'sara',
-		age: 22
-	}
+		id: 2,
+		firt_name: 'Sara',
+		last_name : "Doe",
+		email: 'Saradoe@gmail.com'
+	},
+	{
+		id: 3,
+		firt_name: 'Bob',
+		last_name : "Doe",
+		email: 'Bobdoe@gmail.com'
+	} 
 ]
 
 app.get('/', function(req,res) {
 	var title = 'Customers';
 	res.render('indes',{
-		title : title
+		title : title,
+		users : people
 	});// body...
 });
+
+
+app.post('/user/add' , function(req,res){
+
+	req.checkBody('firt_name', 'First Name is Required').notEmpty();
+	req.checkBody('last_name', 'Last Name is Required').notEmpty();
+	req.checkBody('email', 'Email is Required').notEmpty();
+
+
+	var errors  = req.validationErrors();
+
+	if(errors){
+		var title = 'Customers';
+		res.render('indes',{
+			title : title,
+			users : people,
+			errors : errors
+		});
+	}else{
+
+		var newUser = {
+			firt_name : req.body.firt_name,
+			last_name : req.body.last_name,
+			email : req.body.email
+	}
+
+		console.log("Success");
+	}
+});
+
 
 app.listen(3000,function 
 	(argument) {
