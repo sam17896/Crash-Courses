3381de862ac6152fc5d387a737a87c3ee59ea436
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-12 16:47:40 +0500

    routing

--- Crash Courses/Node.js/Angular/My App/src/app/app.component.ts
+++ Crash Courses/Node.js/Angular/My App/src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <user></user>
+  <ul>
+    <li><a routerLink="/">Home</a></li>
+    <li><a routerLink="/about">About</a></li>
+  </ul>
+  <router-outlet></router-outlet>
   `,
 })
 export class AppComponent  {
