4df9d2e7a01ef982645046dcb3050244b2dfec6b
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-20 18:58:17 +0500

    Typescript: Types

--- Typescript/types.js
+++ Typescript/types.js
@@ -1,22 +1,16 @@
-let myString : string;
-let myNum : number;
-let myBool : boolean;
-let myVar : any;
-let strArray : string[];
-
-
-myString = 'Hello World'.slice(0,3);
+var myString;
+var myNum;
+var myBool;
+var myVar;
+var strArray;
+myString = 'Hello World'.slice(0, 3);
 myNum = 5.5;
 myBool = false;
 myVar = "hello world";
 strArray = ['hello', 'world'];
-
-let strNumTuple : [string, number];
-
+var strNumTuple;
 strNumTuple = ['Hello', 3];
-
 console.log(myNum);
 console.log(myString);
 console.log(myBool);
 console.log(myVar);
-
