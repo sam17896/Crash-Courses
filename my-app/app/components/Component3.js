14e2029e9ce71a84cf0dd494b0b7a7b5f987d076
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-22 19:03:41 +0500

    React-Native: Switch

--- my-app/app/components/Component3.js
+++ my-app/app/components/Component3.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { AppRegistry, Text, View,StyleSheet, TextInput } from 'react-native';
+import { AppRegistry, Text, View,StyleSheet, TextInput, Switch } from 'react-native';
 
 export default class Component3 extends React.Component {
 
     constructor(){
         super();
         this.state = {
-            value : "Hello World"
+            value : "Hello World",
+            switchValue : false
         }
     }
 
     onChangeText(value){
         this.setState({
             value : value
+        });
+    }
+
+    onSwitchChange(value){
+        this.setState({
+            switchValue : value
         });
     }
   
@@ -24,6 +31,10 @@ export default class Component3 extends React.Component {
          value = {this.state.value}
          onChangeText={(value)=>this.onChangeText(value)} />
          <Text>{this.state.value}</Text>
+         <Switch 
+            value = {this.state.switchValue}
+            onValueChange = {(value) => this.onSwitchChange(value)}
+         />
     </View>
     );
   }
