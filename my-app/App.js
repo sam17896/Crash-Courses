f3c31b80cf64a8b6211b7d7b2a0f6a96544251ae
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-22 19:21:44 +0500

    React-Native : ListView

--- my-app/App.js
+++ my-app/App.js
@@ -3,6 +3,7 @@ import { AppRegistry, Text, View,StyleSheet } from 'react-native';
 import Component1 from './app/components/Component1';
 import Component2 from './app/components/Component2';
 import Component3 from './app/components/Component3';
+import Component4 from './app/components/Component4';
 
 export default class App extends React.Component {
 
@@ -13,6 +14,7 @@ export default class App extends React.Component {
       <Component1 message="Hello World"/>
       <Component2 />
       <Component3 />
+      <Component4 />
     </View>
     );
   }
