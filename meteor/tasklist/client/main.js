742d5100f5cf48b7283c8e59aa08246a347f4299
Ahsan Sohail <k142109@nu.edu.pk>
2017-06-27 12:37:09 +0500

    Meteor : Add Task

--- meteor/tasklist/client/main.js
+++ meteor/tasklist/client/main.js
@@ -3,20 +3,25 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Tasks = new Mongo.Collection('tasks');
+
+Template.tasks.helpers({
+  tasks : function(){
+    return Tasks.find({}, {sort: {created_at: -1}});
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.tasks.events({
+  "submit .add-task" : function(event){
+    var name = event.target.name.value;
+    
+    Tasks.insert({
+      name: name,
+      created_at : new Date()
+    });
+    
+    
+    event.target.name.value = "";
+    return false;
+  } 
 });
