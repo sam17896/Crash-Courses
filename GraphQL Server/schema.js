22a1f40f3c68d75590c3b1c71ec669b8a1bb7538
Ahsan Sohail <k142109@nu.edu.pk>
2017-08-08 00:05:15 +0500

    GraphQL : JSON Server and Axios

--- GraphQL Server/schema.js
+++ GraphQL Server/schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const{
     GraphQLObjectType,
     GraphQLString,
@@ -6,16 +7,6 @@ const{
     GraphQLList,
     GraphQLNonNull
 } = require('graphql');
-
-
-//Hardcoded data 
-const customers = [
-    {id:'1',name:'John Doe',email:'jdoe@gmail.com',age:35},
-    {id:'2',name:'Steve Smith',email:'ssmith@gmail.com',age:45},
-    {id:'3',name:'Sara Wilson',email:'swilson@gmail.com',age:25}
-];
-
-
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -39,17 +30,21 @@ const RootQuery = new GraphQLObjectType({
                 id:{type:GraphQLString}
             },
             resolve(parentValue,args){
-                for(let i=0;i<customers.length;i++){
+              /*  for(let i=0;i<customers.length;i++){
                     if(customers[i].id === args.id){
                         return customers[i];
                     }
                 }
+                */
+                return axios.get('http://localhost:3000/customers/' + args.id)
+                        .then(res=>res.data);
             }
         },
         customers:{
             type:new GraphQLList(CustomerType),
             resolve(parentValue,args){
-                return customers;
+                return axios.get('http://localhost:3000/customers')
+                        .then(res=>res.data);
             }
         }
     }
