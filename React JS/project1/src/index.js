833134d46a51500710dd86a43c8488f0d5d0bdb2
Ahsan Sohail <k142109@nu.edu.pk>
2017-08-08 23:18:32 +0500

    babel setup

---
+++ React JS/project1/src/index.js
@@ -0,0 +1,18 @@
+const {createElement} = React;
+const {render} = ReactDOM;
+
+
+const style = {
+    backgroundColor : 'orange',
+    color : 'white',
+    fontFamily : 'verdana'
+}
+
+render(
+    <h1 id='title'
+        className='header'
+        style={style}>
+        Hello World
+    </h1>,
+    document.getElementById('react-native')
+)
