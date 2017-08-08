b5d614e742b463771576a798c16d28f358053e5b
Ahsan Sohail <k142109@nu.edu.pk>
2017-08-09 00:32:58 +0500

    SkiDayCount Component

---
+++ React JS/exercisecounter/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+import {SkiDayCount} from './components/SkiDayCount'
+
+
+render(
+	<SkiDayCount />,
+	document.getElementById('react-container')
+)
