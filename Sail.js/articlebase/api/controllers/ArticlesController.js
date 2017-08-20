23c7d78f6c71abb029edb84060f085ef9a255df1
Ahsan Sohail <k142109@nu.edu.pk>
2017-08-20 20:17:31 +0500

    Sail.js : Add

--- Sail.js/articlebase/api/controllers/ArticlesController.js
+++ Sail.js/articlebase/api/controllers/ArticlesController.js
@@ -6,6 +6,27 @@
  */
 
 module.exports = {
-	
+	list : function(req,res){
+        Articles.find({}).exec(function(err, articles){
+            if(err){
+                res.send(500, {error: 'Database Error'});
+            }
+            res.view('list',{articles:articles});
+        });
+    },
+    add : function(req,res){
+        res.view('add');
+    },
+    create : function(req,res){
+        var title=req.body.title;
+        var body=req.body.body;
+        Articles.create({title:title,body:body}).exec(function(err){
+            if(err){
+                res.send(500,{error:'Database Error'});
+            }
+
+            res.redirect('/articles/list');
+        });
+    }
 };
 
