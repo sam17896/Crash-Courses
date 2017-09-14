d0be410dfb97c2fc382b4c0f465218086bf5ff24
Ahsan Sohail <k142109@nu.edu.pk>
2017-09-14 23:21:36 +0500

    simple html  modal

---
+++ HTMLModal/main.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('modalBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click',openModal);
+
+function openModal(){
+    modal.style.display = 'block';
+}
+
+closeBtn.addEventListener('click',closeModal);
+
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+window.addEventListener('click',outsideClick);
+
+function outsideClick(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
