2019a7f6fd853366da2248cda38f4e6dab6d2c07
Ahsan Sohail <k142109@nu.edu.pk>
2017-09-29 17:24:59 +0500

    NodeMailer : Bring in markup from contact app

---
+++ contactformapp/app.js
@@ -0,0 +1,25 @@
+const express =require('express');
+const bodyParser = require('body-parser');
+const exphbs =require('express-handlebars');
+const path = require('path');
+const nodemailer= require('nodemailer');
+
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use('/public',express.static(path.join(__dirname,'public')));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+app.get('/',(req,res)=>{
+    res.render('contact');
+});
+
+app.listen(3000,()=>{
+    console.log('server started!!');
+})
